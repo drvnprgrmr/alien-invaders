3951d742fb44a99e2fdbd0382360bf509af86fec
qwerty <ekunweibukun159357@gmail.com>
2021-11-05 17:16:56 +0100

    Drawing ship to the midbottom of the screen

--- alien_invaders.py
+++ alien_invaders.py
@@ -1,7 +1,10 @@
+# External imports.
 import sys
-
 import pygame
 
+# Internal imports.
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """ Class modelling the game 'Alien Invaders'."""
@@ -9,18 +12,33 @@ class AlienInvasion:
         """ Initialize the game and game attributes. """
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1000, 600))
-        pygame.display.set_caption('Alien Invaders', 'AI')
+        self.sett = Settings()
 
+        self.screen = pygame.display.set_mode(
+            (self.sett.screen_width, self.sett.screen_height)
+        )
+        pygame.display.set_caption('Alien Invaders', 'AI')
+        self.bg_color = self.sett.bg_color
+
+        self.ship = Ship(self)
+
+    # noinspection PyMethodMayBeStatic
     def run_game(self):
         """ Begin the main loop of the game. """
         while True:
-            # Display the most recent screen.
-            pygame.display.flip()
             for event in pygame.event.get():
                 # Watch for keyboard and mouse events.
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Fill the screen with the set background color
+            self.screen.fill(self.bg_color)
+
+            # Draw the ship to the screen.
+            self.ship.blitme()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """ This class stores all the settings for pygame. """
+    def __init__(self):
+        """ Initialize all the game's settings. """
+        # Screen settings.
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
