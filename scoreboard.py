548f43104d3dc3cb70e072d7771e79211c1cd28a
qwerty <ekunweibukun159357@gmail.com>
2021-11-26 01:08:56 +0100

    Fixed bug with high score.

--- scoreboard.py
+++ scoreboard.py
@@ -32,7 +32,7 @@ class Scoreboard:
 
     def prep_high_score(self):
         """ Load the high score. """
-        high_score = self.stats.high_score
+        high_score = round(self.stats.high_score, -1)
         high_score_str = "High Score: " "{:,}".format(high_score)
 
         self.high_score_image = self.font.render(high_score_str, True,
